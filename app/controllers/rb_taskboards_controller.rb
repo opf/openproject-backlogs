d62f57856e17112bba818a0389e0748efd32668f
Philipp Tessenow <philipp@tessenow.org>
2014-02-24 17:22:57 +0100

    fix #2674 - wrong order of statuses in taskboard

--- app/controllers/rb_taskboards_controller.rb
+++ app/controllers/rb_taskboards_controller.rb
@@ -41,7 +41,7 @@ class RbTaskboardsController < RbApplicationController
   helper :taskboards
 
   def show
-    @statuses     = Type.find_by_id(Task.type).statuses
+    @statuses     = Type.find_by_id(Task.type).statuses.order(:position)
     @story_ids    = @sprint.stories(@project).map{|s| s.id}
     @last_updated = Task.find(:first,
                               :conditions => ["parent_id in (?)", @story_ids],
